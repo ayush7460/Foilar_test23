da7c189a60c53e70e70b386b91b6e244a485d55e
ayush gupta <143194898+ayush7460@users.noreply.github.com>
2025-01-24 00:22:39 +0530

    45

--- server.js
+++ server.js
@@ -10,12 +10,11 @@ const signup = require('./Routes/user_auth/signup');
 const login = require('./Routes/user_auth/login');
 const otp = require('./Routes/user_auth/otp');
 const profile = require('./Routes/user_auth/profile');
-const customer = require('./Routes/customer/customer');
+
 const supplier = require('./Routes/supplier/supplier');
 const suppconnect = require('./Routes/supplier/supp-connect');
-const transactions = require('./Routes/customer/transaction');
-const chat = require('./Routes/customer/chat');
-const c_detail = require('./Routes/customer/C_detail')
+
+
 const s_detail = require('./Routes/supplier/S_detail');
 const seebycustomer = require('./Routes/look_by_customer/see_by_customer')
 const connect = require('./Routes/look_by_customer/connect');
@@ -32,11 +31,11 @@ app.use('/api', signup);
 app.use('/api', login);  
 app.use('/api', otp);
 app.use('/api', profile);
-app.use('/api', c_detail);
+
 app.use('/api', s_detail);
-app.use('/api', customer);
-app.use('/api', transactions);
-app.use('/api', chat);
+
+
+
 
 app.use('/api', supplier);
 app.use('/api', suppconnect);
