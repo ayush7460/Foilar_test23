c79b3923cca4296b7f31f40122fe83d2142b117d
ayush gupta <143194898+ayush7460@users.noreply.github.com>
2025-01-24 18:39:07 +0530

    /

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,21 +1,21 @@
-// const jwt = require('jsonwebtoken');
-// const authenticateUser = async (req, res, next) => {
-//   const token = req.header('x-auth-token');
+const jwt = require('jsonwebtoken');
+const authenticateUser = async (req, res, next) => {
+  const token = req.header('x-auth-token');
 
-//   if (!token) {
-//     return res.status(401).json({ error: 'Authorization denied. No token provided.' });
-//   }
+  if (!token) {
+    return res.status(401).json({ error: 'Authorization denied. No token provided.' });
+  }
 
-//   try {
-//     const decoded = jwt.decode(token);
+  // try {
+  //   const decoded = jwt.decode(token);
 
-//     req.userId = decoded.userId;
-//     req.ByPhoneNumber =  decoded.mobileNumber
-//     next();
-//   } catch (err) {
-//     console.error('Token verification failed:', err);
-//     res.status(401).json({ error: 'Token is not valid.' });
-//   }
-// };
+  //   req.userId = decoded.userId;
+  //   req.ByPhoneNumber =  decoded.mobileNumber
+  //   next();
+  // } catch (err) {
+  //   console.error('Token verification failed:', err);
+  //   res.status(401).json({ error: 'Token is not valid.' });
+  // }
+};
 
-// module.exports = { authenticateUser };
+module.exports = { authenticateUser };
