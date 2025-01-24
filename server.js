62e3225991fed7eff8ed8b88aa8acc7bd6134a59
ayush gupta <143194898+ayush7460@users.noreply.github.com>
2025-01-24 11:47:02 +0530

    new start

--- server.js
+++ server.js
@@ -1,40 +1,57 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
-
+const mongoose = require('mongoose');
+// const Customer = require('./models/Customer');
+// const Supplier = require('./models/Supplier');
 const { v4: uuidv4 } = require('uuid');
 
-
-
-
-
-// const seebycustomer = require('./Routes/look_by_customer/see_by_customer')
-// const connect = require('./Routes/look_by_customer/connect');
-
+const signup = require('./Routes/user_auth/signup');
+const login = require('./Routes/user_auth/login');
+const otp = require('./Routes/user_auth/otp');
+const profile = require('./Routes/user_auth/profile');
+const customer = require('./Routes/customer/customer');
+const supplier = require('./Routes/supplier/supplier');
+const suppconnect = require('./Routes/supplier/supp-connect');
+const transactions = require('./Routes/customer/transaction');
+const chat = require('./Routes/customer/chat');
+const c_detail = require('./Routes/customer/C_detail')
+const s_detail = require('./Routes/supplier/S_detail');
+const seebycustomer = require('./Routes/look_by_customer/see_by_customer')
+const connect = require('./Routes/look_by_customer/connect');
+const loanRoutes = require('./Routes/Loans/loanRoutes');
+const loanProfile = require('./Routes/Loans/loan-profile');
+const add = require('./Routes/Loans/add');
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
+app.use(cors({ origin: 'http://localhost:3000' }));
+mongoose.set('debug', true);
 
-app.use(cors({ origin: 'https://transaction-top.vercel.app' }));
+app.use('/api', signup);
+app.use('/api', login);  
+app.use('/api', otp);
+app.use('/api', profile);
+app.use('/api', c_detail);
+app.use('/api', s_detail);
+app.use('/api', customer);
+app.use('/api', transactions);
+app.use('/api', chat);
+
+app.use('/api', supplier);
+app.use('/api', suppconnect);
+
+app.use('/api', seebycustomer);
+app.use('/api', connect);
+
+app.use('/api', add);
+app.use('/api', loanRoutes);
+app.use('/api', loanProfile);
 
 
 
-
-
-
-
-
-
-
-
-// app.use('/api', seebycustomer);
-// app.use('/api', connect);
-
-
-
-
-
-
+let customers = [];
+let suppliers = [];
 
 // Connect to MongoDB mongodb+srv://ad:a1y2u3@cluster0.y0axid7.mongodb.net
 // mongoose.connect('mongodb+srv://ad:a1y2u3@cluster0.y0axid7.mongodb.net/FOILAR?retryWrites=true&w=majority', {
@@ -47,15 +64,14 @@ app.use(cors({ origin: 'https://transaction-top.vercel.app' }));
 //   console.error('Error stack:', err.stack);
 // });
 
-// mongoose.connect('mongodb+srv://ayush1777:agr11@cluster0.0128p.mongodb.net/FOILAR',{ useNewUrlParser: true, useUnifiedTopology: true });
-// const db = mongoose.connection;
-// db.once('open', () => {
-//   console.log('Connected to MongoDB');
-// });
+mongoose.connect('mongodb+srv://ayush1777:agr11@cluster0.0128p.mongodb.net/FOILAR', { useNewUrlParser: true,  });
+const db = mongoose.connection;
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-// app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static('uploads'));
 
 
 // API endpoints
