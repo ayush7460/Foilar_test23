86e7c4dda59ef2824d4d0a374cecbfcb8b4337a4
ayush gupta <143194898+ayush7460@users.noreply.github.com>
2025-01-24 19:01:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -79,14 +79,14 @@ app.use('/api', loanProfile);
 //   console.error('Error stack:', err.stack);
 // });
 
-mongoose.connect('mongodb+srv://ayush1777:agr11@cluster0.0128p.mongodb.net/FOILAR', { useNewUrlParser: true,  });
-const db = mongoose.connection;
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// mongoose.connect('mongodb+srv://ayush1777:agr11@cluster0.0128p.mongodb.net/FOILAR', { useNewUrlParser: true,  });
+// const db = mongoose.connection;
+// db.once('open', () => {
+//   console.log('Connected to MongoDB');
+// });
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-app.use('/uploads', express.static('uploads'));
+// app.use('/uploads', express.static('uploads'));
 
 
 // API endpoints
