76c8e4f13fa0129964b2309a9e0c86085c718bbd
ayush gupta <143194898+ayush7460@users.noreply.github.com>
2025-01-24 12:10:03 +0530

    middle w

--- middleware/limited_auth.js
+++ middleware/limited_auth.js
@@ -1,22 +1,22 @@
-const jwt = require('jsonwebtoken');
-const limited_auth = async (req, res, next) => {
-  const token = req.header('limited_auth');
+// const jwt = require('jsonwebtoken');
+// const limited_auth = async (req, res, next) => {
+//   const token = req.header('limited_auth');
 
-  if (!token) {
-    return res.status(401).json({ error: 'Authorization denied. No token provided.' });
-  }
+//   if (!token) {
+//     return res.status(401).json({ error: 'Authorization denied. No token provided.' });
+//   }
 
-  try {
-    const decoded = jwt.decode(token);
+//   try {
+//     const decoded = jwt.decode(token);
 
-    req.userId = decoded.adderuserId;
-    req.myphone =  decoded.myphone;
-    req.adderPhoneNumber = decoded.adderPhoneNumber;
-    next();
-  } catch (err) {
-    console.error('Token verification failed:', err);
-    res.status(401).json({ error: 'Token is not valid.' });
-  }
-};
+//     req.userId = decoded.adderuserId;
+//     req.myphone =  decoded.myphone;
+//     req.adderPhoneNumber = decoded.adderPhoneNumber;
+//     next();
+//   } catch (err) {
+//     console.error('Token verification failed:', err);
+//     res.status(401).json({ error: 'Token is not valid.' });
+//   }
+// };
 
-module.exports = { limited_auth };
+// module.exports = { limited_auth };
