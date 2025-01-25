1d5fbe476a3bfa348f932bec73cc61fd409705e3
ayush gupta <143194898+ayush7460@users.noreply.github.com>
2025-01-25 21:01:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
 
 // Middleware
