92d02ea03faea7e88dff8fb69666fdbff21ad246
ayush gupta <143194898+ayush7460@users.noreply.github.com>
2025-01-25 19:20:15 +0530

    \

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config(); 
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -5,17 +6,21 @@ const mongoose = require('mongoose');
 
 
 // Middleware
-app.use(express.json());
 
-mongoose.connect('mongodb+srv://ayush1777:agr11@cluster0.0128p.mongodb.net/FOILAR', { useNewUrlParser: true,  });
+
+const mongoURI = process.env.MONGODB_URI;
+
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.once('open', () => {
   console.log('Connected to MongoDB');
 });
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-app.use('/uploads', express.static('uploads'));
+// app.use('/uploads', express.static('uploads'));
 
+app.use(express.json());
 // Routes
 app.get('/', (req, res) => {
   res.send('Hello, Express!');
