82f9f1e3c24e58b697cbb59a17764fabc08b921e
ayush gupta <143194898+ayush7460@users.noreply.github.com>
2025-01-25 18:37:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+
 
 // Middleware
 app.use(express.json());
+
+mongoose.connect('mongodb+srv://ayush1777:agr11@cluster0.0128p.mongodb.net/FOILAR', { useNewUrlParser: true,  });
+const db = mongoose.connection;
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use('/uploads', express.static('uploads'));
 
 // Routes
 app.get('/', (req, res) => {
@@ -14,3 +25,4 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
