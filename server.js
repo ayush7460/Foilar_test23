bff15e822f5a10d1652935755bae04d58b26bbce
ayush gupta <143194898+ayush7460@users.noreply.github.com>
2025-01-25 12:11:07 +0530

    mini uploade

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.use(cors({ origin: 'http://localhost:3000' }));
 // });
 // db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-app.use('/uploads', express.static('uploads'));
+// app.use('/uploads', express.static('uploads'));
 
 
 const port = 5000;
